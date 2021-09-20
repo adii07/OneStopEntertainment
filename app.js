11ca54d73c57beb21f021534a64c7a25b2b97053
aditya <adityasharma5119@gmail.com>
2021-09-20 05:40:50 +0530

    data fetch

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ function onClickAction(){
     .then(response=> response.json())
     .then(json=>{
         var movieDirector=json.Director
+        var movieYear=json.Year
+        var movieGenre=json.Genre
+        var movieActors=json.Actors
+        var movieAwards=json.Awards
+        var movieRating=json.Ratings[0].Value
+        var moviePlot=json.Plot
+        var moviePoster=json.Poster
+        console.log(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster)
+        setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster)
         console.log(movieDirector)
     })
     .catch(errorHandler)
@@ -23,4 +32,17 @@ function getValidURL(input){
 
 function errorHandler(error) {
     console.log("error occurred" + error)
+}
+
+function setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster){
+  //  $("#hidden").toggle();
+    document.getElementById("movieDirector").innerText=movieDirector
+    document.getElementById("movieYear").innerText=movieYear
+    document.getElementById("movieGenre").innerText=movieGenre
+    document.getElementById("movieActors").innerText=movieActors
+    document.getElementById("movieAwards").innerText=movieAwards
+    document.getElementById("movieRating").innerText=movieRating
+    document.getElementById("moviePlot").innerText=moviePlot
+    document.getElementById("moviePoster").src=moviePoster
+    
 }
