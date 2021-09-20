0fa153b8f17f674b09f9b2ca8a00bbc76cb9e9d6
aditya <adityasharma5119@gmail.com>
2021-09-20 06:10:19 +0530

    seperate landing pages

--- series.js
+++ series.js
@@ -17,9 +17,10 @@ function onClickAction(){
         var movieAwards=json.Awards
         var movieRating=json.Ratings[0].Value
         var moviePlot=json.Plot
+        var movieSeasons=json.totalSeasons
         var moviePoster=json.Poster
-        console.log(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster)
-        setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster)
+        console.log(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster,movieSeasons)
+        setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster,movieSeasons)
     })
     .catch(errorHandler)
 }
@@ -33,7 +34,7 @@ function errorHandler(error) {
     console.log("error occurred" + error)
 }
 
-function setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster){
+function setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,movieRating,moviePlot,moviePoster,movieSeasons){
   //  $("#hidden").toggle();
     document.getElementById("movieDirector").innerText=movieDirector
     document.getElementById("movieYear").innerText=movieYear
@@ -43,5 +44,6 @@ function setValues(movieDirector,movieYear,movieGenre,movieActors,movieAwards,mo
     document.getElementById("movieRating").innerText=movieRating
     document.getElementById("moviePlot").innerText=moviePlot
     document.getElementById("moviePoster").src=moviePoster
+    document.getElementById("movieSeasons").innerText=movieSeasons
     
 }
