412e55736ba7c0cf4761cc5a546e7f686b1c2bb5
aditya <adityasharma5119@gmail.com>
2021-09-29 01:51:12 +0530

    adding genre tags

--- tmdb.js
+++ tmdb.js
@@ -5,6 +5,85 @@ const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
 
 const main = document.getElementById('main');
 const form = document.getElementById('form');
@@ -12,6 +91,21 @@ const search = document.getElementById('search');
 const tagsEl = document.getElementById('tags');
 
 getMovies(API_URL);
+
+getGenre();
+
+
+function getGenre(){
+    tagsEl.innerHTML=''
+
+    genres.forEach(genre=>{
+        const genre_holder=document.createElement('div')
+        genre_holder.classList.add('tags')
+        genre_holder.id=genre.id
+        genre_holder.innerText=genre.name
+        tagsEl.append(genre_holder)
+    })
+}
 
 function getMovies(url) {
     lastUrl = url;
@@ -22,7 +116,7 @@ function getMovies(url) {
             if (data.results.length !== 0) {
                 showMovies(data.results);
             } else {
-                main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+                main.innerHTML = `<h1 class="no-results" style="color: white;">No Results Found</h1>`
             }
 
         })
